dd59db857e31ad91803884ef32955a16d752c12a
Webkunx <stormvanya@gmail.com>
2022-10-06 20:31:06 +0300

    ft: add logger

---
+++ src/common/logger.ts
@@ -0,0 +1,56 @@
+import pino, {
+  Logger as PinoLogger,
+  LoggerOptions as PinoLoggerOptions,
+} from "pino";
+// TO COMMON
+import { ParsedJSON } from "../core/json";
+
+type DataToLog = { message: string; payload?: Record<string, ParsedJSON> };
+
+enum LogLevel {
+  INFO = "info",
+  ERROR = "error",
+  WARN = "warn",
+}
+
+class Logger {
+  private readonly pino: PinoLogger<PinoLoggerOptions>;
+  private constructor(private readonly name: string) {
+    this.pino = pino(this.getConfig());
+  }
+
+  [LogLevel.ERROR](data: DataToLog) {
+    this.log(LogLevel.ERROR, data);
+  }
+  [LogLevel.INFO](data: DataToLog) {
+    this.log(LogLevel.INFO, data);
+  }
+  [LogLevel.WARN](data: DataToLog) {
+    this.log(LogLevel.WARN, data);
+  }
+
+  private getConfig() {
+    if (process.env.ENV === "DEV") {
+      return {
+        transport: {
+          target: "pino-pretty",
+        },
+      };
+    }
+    return {};
+  }
+  private log(level: LogLevel, data: DataToLog) {
+    this.pino[level]({ ...data.payload, loggerName: this.name }, data.message);
+  }
+
+  static loggers: Record<string, Logger> = {};
+
+  static getLogger(name: string = "app-logger") {
+    if (!(name in this.loggers)) {
+      this.loggers[name] = new Logger(name);
+    }
+    return this.loggers[name];
+  }
+}
+
+Logger.getLogger().error({ message: "Shiiet", payload: { yes: true } });
